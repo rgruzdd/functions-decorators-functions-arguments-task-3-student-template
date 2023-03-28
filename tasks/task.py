f38b115e421bc05faf85c6ad0704813cb897192a
Gruzin <gruzd.roman@inbox.ru>
2023-03-28 19:18:26 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -10,16 +10,3 @@ def combine_dicts(*args:List[Dict[str, int]]) -> Dict[str, int]:
                 new_dict[k] = i[k]
     return new_dict
 
-
-
-dict_1 = {'a': 100, 'b': 200}
-dict_2 = {'a': 200, 'c': 300}
-dict_3 = {'a': 300, 'd': 100}
-
-
-
-#print(combine_dicts(dict_1, dict_2))
-# >>> {'a': 300, 'b': 200, 'c': 300}
-
-print(combine_dicts(dict_1, dict_2, dict_3))
-# >>> {'a': 600, 'b': 200, 'c': 300, 'd': 100}
